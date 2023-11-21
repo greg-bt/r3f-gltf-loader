5aa3871427373475c8338977f2b667103fa73bc5
Greg <52139156+greg-bt@users.noreply.github.com>
2023-11-21 20:54:06 +0000

    Inital Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,49 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
+import { Canvas, MeshProps, useLoader } from '@react-three/fiber'
+import { Environment, OrbitControls } from '@react-three/drei';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+interface ModelProps extends MeshProps {
+  url: string
+}
+
+function Model({ url, ...props } : ModelProps) {
+  const gltf : any = useLoader(GLTFLoader, url);
+  return <primitive object={gltf.scene} {...props} />
+}
+
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  return <>
+    <VRButton />
+      <Canvas>
+        <XR>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          { /* Lighting */}
+          <Environment background files={"./environment.hdr"} />
+          <directionalLight
+            position={[50,180,50]}
+            color={0xFFFAd8}
+            intensity={4.5}
+            castShadow
+            shadow-bias={-0.0001}
+          />
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+          { /* Controls */}
+          <OrbitControls makeDefault />
+          <Controllers />
+          <Hands />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          { /* Objects */}
+          <gridHelper args={[100, 100]} />
+
+          <Model url="./Monitor.gltf" scale={[2,2,2]} />
+          
+
+        </XR>
+      </Canvas>
+  </>
 }
